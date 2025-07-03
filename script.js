449452147b54233ae52f096c94b199fca5c12b32
nborsch <36388040+nborsch@users.noreply.github.com>
2025-07-03 13:12:50 -0500

    Build UI

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-/* Get computer choice first*/
+const rockBtn = document.querySelector('#rock')
+const paperBtn = document.querySelector('#paper')
+const scissorsBtn = document.querySelector('#scissors')
+let computerScore = 0
+let humanScore = 0
+/* when user clicks it has already gone through a full round */
+let rounds = 1 
+
+/* Listening for a button choice, calling playRound */
+document.addEventListener('click', (e) => {
+    const choice = e.target.textContent
+
+    if (choice === 'rock' 
+        || choice === 'paper'
+        || choice === 'scissors') playRound(choice)
+})
+
 function getComputerChoice() {
     const randomNumberOfThree = Math.floor(Math.random() * 3) + 1
     return randomNumberOfThree === 1 ? 'rock' : randomNumberOfThree === 2 ? 'paper' : 'scissors'
 }
 
-/*Prompt user for human choice*/
-function getHumanChoice() {
-    return prompt('rock, paper, or scissors?').toLowerCase()
+function playRound(playerSelection) {
+    const totalWinner = document.querySelector('.winner')
+    const currentRoundWinner = document.querySelector('.current-round-winner')
+    const currentRound = document.querySelector('.current-round')
+    const computerChoiceDisplay = document.querySelector('.computer-choice')
 
-}
+    const computerChoice = getComputerChoice();
 
-function playGame() {
-    let computerScore = 0
-    let humanScore = 0
+    /* Reset rounds once 5 rounds have been played */
+    if (rounds === 6) rounds = 1
 
-    /*Game logic*/
-    function playRound() {
+    currentRound.textContent = rounds
+    computerChoiceDisplay.textContent = computerChoice
+    
+    /* Game is a tie */
+    if (playerSelection === computerChoice) {
+        currentRoundWinner.textContent = `It's a tie!`
+    /* Human wins */
+    } else if ((playerSelection === 'paper' && computerChoice === 'rock') ||
+        (playerSelection === 'scissors' && computerChoice === 'paper') ||
+        (playerSelection === 'rock' && computerChoice === 'scissors')) {
 
-        const computerSelection = getComputerChoice();
-        const humanSelection = getHumanChoice();
-
-        /* Game is a tie */
-        if (humanSelection === computerSelection) {
-            console.log(`It's a tie! Nobody wins.`)
-
-            /* Human wins */
-        } else if ((humanSelection === 'paper' && computerSelection === 'rock') ||
-            (humanSelection === 'scissors' && computerSelection === 'paper') ||
-            (humanSelection === 'rock' && computerSelection === 'scissors')) {
-
-            console.log(`You win! ${humanSelection.charAt(0).toUpperCase() +
-                humanSelection.slice(1)} beats ${computerSelection}.`)
-
-            humanScore++
-
-            /* Human loses */
-        } else {
-            console.log(`You lose! ${computerSelection.charAt(0).toUpperCase() +
-                computerSelection.slice(1)} beats ${humanSelection}.`)
-
-            computerScore++
-        }
+        currentRoundWinner.textContent = 'You'
+        humanScore++
+    /* Human loses */
+    } else {
+        currentRoundWinner.textContent = 'Computer'
+        computerScore++
     }
 
-    for (let i = 5; i > 0; i--) {
-        playRound()
+    /* If 5 rounds were played, announce the winner */
+    if (rounds === 5) {
+        totalWinner.textContent = computerScore === humanScore ? `It's a tie!` : computerScore > humanScore ? 'Computer' : 'You'
+    } else {
+        totalWinner.textContent = ''
     }
-
-    const winner = computerScore === humanScore ? `No winner, it's a tie!` : computerScore > humanScore ? `The computer wins!` : `You're the final winner!`
-
-    /* Show total game rounds result*/
-    console.log(`${winner} Computer ${computerScore} You ${humanScore}`)
+    /* Played a full round */
+    rounds++
 }
